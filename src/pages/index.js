7a1331dbbd549da831de684f6d288050379511d0
Eveutida <evellyn.utida@estudante.ifms.edu.br>
2024-09-30 14:14:47 +0000

    aula 30/09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,31 @@
 import Footer from "./modulos/footer";
 import Navbar from "./modulos/navbar";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function Index() {
 
   return <div> 
     <Navbar/>
      Bem-vindo 
+    <Container>
+      <Row>
+        <Col className="bg-primary ">1 of 2</Col>
+        <Col className="bg-primary-subtle">2 of 2</Col>
+      </Row>
+      <Row>
+        <Col className="bg-danger">1 of 3</Col>
+        <Col className="bg-danger-subtle">2 of 3</Col>
+        <Col className="bg-warning-subtle">3 of 3</Col>
+      </Row>
+    </Container>
     <Footer/>
   </div>
 }
+
+
+
 export default Index;
+
+
