47b0c628201dcb1ad32b6f8b7aa777ebff33955d
Eveutida <evellyn.utida@estudante.ifms.edu.br>
2024-11-29 11:53:20 +0000

    29-11

---
+++ src/pages/pesportes.js
@@ -0,0 +1,39 @@
+import { Container, Table } from "react-bootstrap";
+import Menu from "./modulos/navbar";
+import Footer from "./modulos/footer";
+
+export default function Pesportes() {
+    return <>
+        <Menu />
+        <Container>
+            <Container className="border border-success rounded-pill text-center col-sm-6 m-2">
+                Lista de Produtos de Esportes
+            </Container>
+            <Container>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Produto</th>
+                            <th>Categoria </th>
+                            <th>Descrição</th>
+                            <th>Preço</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <th></th>
+                        </tr>
+                    </tbody>
+                </Table>
+
+            </Container>
+        </Container>
+        <Footer />
+    </>
+}
